b9b76887b63a4aa1481cb557239452ba47956b31
Rajudev <rajussp31121997@gmail.com>
2022-03-31 15:25:12 +0530

    stump physics added

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e0947a7d352df5e787d011a41f5c108343d62d0f4fd6ef1d7da253ce2beb7bc
-size 1668
+oid sha256:44d3aa0d22d28b148d05ec1b09bfc8d21993bfa83ae28297ba98ff2bbfe15596
+size 851

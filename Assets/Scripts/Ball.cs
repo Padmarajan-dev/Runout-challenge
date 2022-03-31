4de588d9799a9ce1d0aa62a198ed69a63e8c5b73
Rajudev <rajussp31121997@gmail.com>
2022-03-31 16:59:45 +0530

    break stump intiated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44d3aa0d22d28b148d05ec1b09bfc8d21993bfa83ae28297ba98ff2bbfe15596
-size 851
+oid sha256:2f09dce4719d6489340840416f383913b8f505680891fd66b51be6ead999b317
+size 936

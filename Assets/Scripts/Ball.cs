a41f887eb1115415cc5eb612ae44a3cd3c12ca29
Rajudev <rajussp31121997@gmail.com>
2022-04-01 18:25:57 +0530

    break the stump Ui Initiated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b55f95122ab9c493431d2749c37859d5f664006a1c129ede1c4fd813c0138df
-size 1001
+oid sha256:e5565be2898f5b0584943029d0fb2cc683c16e4a8b6482d56470c5a382b7dbf8
+size 1030
--- Assets/Scripts/Stump.cs
+++ Assets/Scripts/Stump.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0044e52abb6fbf6838f25c4ed75abcbcdd4597308e53665e16f98376eaf64a67
-size 640
+oid sha256:5f6b80ccc3f3b5a63c69c9fadcfd3451755c0b185d197992f00cacd347c91a8e
+size 731
